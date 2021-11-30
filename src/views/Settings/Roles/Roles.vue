e697718d1dba47828327bead3a294648053e93a8
ammduncan <ammielgyanyawson@gmail.com>
2021-11-30 09:29:08 +0000

    import settings styles in roles styles

--- src/views/Settings/Roles/Roles.vue
+++ src/views/Settings/Roles/Roles.vue
@@ -1,36 +1,36 @@
 <template>
   <div class="settings__content">
+    <h1>Roles & Permissions</h1>
     <div class="settings__section settings__section--with-line">
-      <k-card heading="Roles & Permissions" variant="no-space-top">
-        <editable-list-item
-          :show-controls="editing"
-          v-for="role in roles"
-          :key="role.title"
-          :styling="{ marginBottom: '3.2rem' }"
-          :no-delete="role.noDelete"
-          :no-edit="role.noEdit"
-          :item="role"
-          @edit="editItem"
-          @delete="deleteItem"
-        >
-          <div class="role">
-            <h3 class="role__title">
-              {{ role.title }}
-            </h3>
-            <div class="role__description">{{ role.description }}</div>
-          </div>
-        </editable-list-item>
-        <div class="controls">
-          <k-button variant="tertiary" @click="editing = true" v-if="!editing">Edit Roles</k-button>
-          <template>
-            <k-button variant="link" v-if="editing" @click='addItem'>Add New Role</k-button>
-            <k-button variant="tertiary" @click="editing = false" v-if="editing">
-              Save Changes
-            </k-button>
-          </template>
+      <editable-list-item
+        :show-controls="editing"
+        v-for="role in roles"
+        :key="role.title"
+        :styling="{ marginBottom: '3.2rem' }"
+        :no-delete="role.noDelete"
+        :no-edit="role.noEdit"
+        :item="role"
+        @edit="editItem"
+        @delete="deleteItem"
+      >
+        <div class="role">
+          <h3 class="role__title">
+            {{ role.title }}
+          </h3>
+          <div class="role__description">{{ role.description }}</div>
         </div>
-      </k-card>
-      <k-modal :open="modalOpen">
+      </editable-list-item>
+      <div class="controls">
+        <k-button variant="tertiary" @click="editing = true" v-if="!editing">Edit Roles</k-button>
+        <template>
+          <k-button variant="link" v-if="editing" @click="addItem">Add New Role</k-button>
+          <k-button variant="tertiary" @click="editing = false" v-if="editing">
+            Save Changes
+          </k-button>
+        </template>
+      </div>
+      <!--      </k-card>-->
+      <k-modal :open="modalOpen" @close="modalOpen = false">
         <role-form :edit="role" @close="modalOpen = false" />
       </k-modal>
     </div>
