e4b31868fb7920fa8fa5b5557139ece1713b7792
ammduncan <ammielgyanyawson@gmail.com>
2021-12-10 15:46:43 +0000

    resolve conflicts

--- tests/utils/local-vue.js
+++ tests/utils/local-vue.js
@@ -9,7 +9,7 @@ localVue.use(Toast);
 const successStore = new Vuex.Store({
   modules: {
     auth: {
-      getters: {
+      getters: {on''
         getUser: () => ({
           firstName: 'Akomolafe',
           lastName: 'Oluwaseun',
