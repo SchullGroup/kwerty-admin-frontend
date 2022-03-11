dbf6c8d52339a26cb00b08a6afcd9489a4d66f55
Ammiel Yawson <ammielgyanyawson@gmail.com>
2022-03-11 17:05:39 +0000

    add express server at port 6784

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 const app = express();
 const path = `${__dirname}/dist/`;
-const port = process.env.PORT || 6783;
+const port = process.env.PORT || 6784;
 
 app.use(compression({ level: 9 }));
 app.use(express.static(path));
